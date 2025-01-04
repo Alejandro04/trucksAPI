7b8ab9b6569f02c42a83bf25547a7a41828fa1d8
Alejandro04 <alejodev04@gmail.com>
2025-01-04 15:02:48 -0400

    add get_carriers mock endpoint

---
+++ app/app.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request
+from enum import Enum
+
+app = Flask(__name__)
+
+class City(Enum):
+    NEW_YORK = "New York"
+    WASHINGTON_DC = "Washington DC"
+    SAN_FRANCISCO = "San Francisco"
+    LOS_ANGELES = "Los Angeles"
+    MIAMI = "Miami"
+    CHICAGO = "Chicago"
+
+mock_carriers = {
+    "NY-DC": [
+        {"name": "Knight-Swift Transport Services", "trucks_per_day": 10},
+        {"name": "J.B. Hunt Transport Services Inc", "trucks_per_day": 7},
+        {"name": "YRC Worldwide", "trucks_per_day": 5},
+    ],
+    "SF-LA": [
+        {"name": "XPO Logistics", "trucks_per_day": 9},
+        {"name": "Schneider", "trucks_per_day": 6},
+        {"name": "Landstar Systems", "trucks_per_day": 2},
+    ],
+    "default": [
+        {"name": "UPS Inc.", "trucks_per_day": 11},
+        {"name": "FedEx Corp", "trucks_per_day": 9},
+    ],
+}
+
+@app.route('/get-carriers', methods=['GET'])
+def get_transporters():
+    from_city = request.args.get('from_city', '').strip()
+    to_city = request.args.get('to_city', '').strip()
+
+    try:
+        from_city_enum = City(from_city)
+        to_city_enum = City(to_city)
+    except ValueError:
+        return jsonify({"error": "Una o ambas ciudades no son válidas."}), 400
+
+    if from_city_enum == City.NEW_YORK and to_city_enum == City.WASHINGTON_DC:
+        transporters = mock_carriers["NY-DC"]
+    elif from_city_enum == City.SAN_FRANCISCO and to_city_enum == City.LOS_ANGELES:
+        transporters = mock_carriers["SF-LA"]
+    else:
+        transporters = mock_carriers["default"]
+
+    return jsonify(transporters)
+
+if __name__ == '__main__':
+    app.run(debug=True)
